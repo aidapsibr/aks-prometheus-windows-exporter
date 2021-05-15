78344fa11a70d77d0bd4b880ce747af6a4854743
AIDA <aida@psibr.net>
2021-05-15 13:58:09 -0700

    Create aks_setup.ps1

---
+++ aks_setup.ps1
@@ -0,0 +1,32 @@
+# Contains initial configuration for skykick windows aks nodes
+# This is the file that is supplied to powershell dsc so it knows what to do
+
+Configuration Setup
+{
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Node localhost
+    {
+        Script Kubelet
+        {
+            GetScript = { return; }            
+            TestScript = { return $false; }
+            SetScript = 
+            {
+                $ErrorActionPreference = 'Stop';
+
+                # This sets up prometheus windows node exporter on the node. 
+                # Note: there is a second step needed for prometheus to actually pull from the node exporter by adding this nodes ip and port to the node exporter endpoint in k8s.
+                #       This will have to happen in another place that keeps that list maintained based on the windows nodes available in aks
+                function Setup-Windows-Exporter
+                {
+                    msiexec /i `
+                        C:\PROGRA~1\WindowsPowerShell\Modules\sk_dsc_resources\windows_exporter-0.16.0-amd64.msi `
+                        LISTEN_PORT=9100 `
+                        ENABLED_COLLECTORS=cpu,cs,container,logical_disk,memory,net,os,service,system,tcp
+                }
+                
+                Setup-Windows-Exporter;
+            }
+        }
+    }
+}
